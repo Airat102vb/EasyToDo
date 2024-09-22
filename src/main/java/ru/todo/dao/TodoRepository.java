7730a6cd1435e99345fd234777dd5d722e0b681a
Airat <airat102vb@mail.com>
2024-09-22 21:41:12 +0300

    upd

--- src/main/java/ru/todo/dao/TodoRepository.java
+++ src/main/java/ru/todo/dao/TodoRepository.java
@@ -1,12 +1,14 @@
 package ru.todo.dao;
 
+import java.sql.*;
+import java.util.LinkedList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import javax.sql.DataSource;
-import java.sql.*;
+import ru.todo.dto.Todo;
 
 @Component
 public class TodoRepository {
@@ -33,6 +35,33 @@ public class TodoRepository {
       } else {
         return "nothing";
       }
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public List<Todo> getTodoEntriesById(int userId) {
+    try {
+      Connection connection = dataSource.getConnection();
+      Statement statement = connection.createStatement();
+      String sql = """
+                   SELECT * from USERS u
+                   JOIN todo t on u.id = t.user_id
+                   WHERE user_id = %d
+                   """.formatted(userId);
+      ResultSet resultSet = statement.executeQuery(sql);
+
+      List<Todo> entries = new LinkedList();
+      while (resultSet.next()) {
+        entries.add(
+            new Todo(
+                resultSet.getString("full_name"),
+                resultSet.getString("login"),
+                resultSet.getString("entry")
+            )
+        );
+      }
+      return entries;
     } catch (SQLException e) {
       throw new RuntimeException(e);
     }
